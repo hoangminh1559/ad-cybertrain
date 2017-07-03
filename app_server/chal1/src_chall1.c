4b7afcedd86178ef2f282ef3b42130c440e7167e
root <root@s325718a4b01a47e9813f7b010d290f180-App1.uitlab.com>
2017-07-03 15:06:48 +0000

    Add existing file

---
+++ app_server/chal1/src_chall1.c
@@ -0,0 +1,51 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+ 
+/*
+gcc -m32 -o ch13 ch13.c -fno-stack-protector
+*/
+ 
+void shell() {
+   printf("Good game well played!\n");
+    system("/bin/cat flag.txt");
+}
+void sup() {
+    printf("\nTurn back ! Wrong way \n");
+}
+void phase2()
+{
+  int var;
+  void (*func)()=sup;
+  char buf[128];
+  fgets(buf,133,stdin);
+  func();
+}
+void phase1()
+{
+  int var;
+  int check = 0x04030201;
+  char buf[40];
+ 
+  fgets(buf,45,stdin);
+  printf("\n[buf]: %s\n", buf );
+  printf("[check] %p\n", check);
+
+  if ((check != 0x04030201) && (check != 0xdeadbeef))
+    printf ("\nYou are on the right way!\n");
+
+  if (check == 0xdeadbeef)
+   {
+     printf("Yeah dude, you win phase 1!\nHere is your flag...\n");
+     system("/bin/cat flag.txt");
+   }
+}
+int main()
+{
+  setvbuf(stdout, NULL, _IONBF, 0);
+  printf("The battle begin !\n");
+  phase1();
+  printf("Fight me !\n");
+  phase2();
+  return 0;
+}
---
+++ app_server/chal2/chall2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <unistd.h>
+ 
+void phase1(char *inp)
+{
+	FILE *secret = fopen("flag.txt", "rt");
+	char buffer[32];
+	fgets(buffer, sizeof(buffer), secret);
+	printf(inp);
+	fclose(secret);
+}
+void phase2(char *inp)
+{
+	int var;
+	int check  = 0x04030201;
+ 
+	char fmt[128];
+	memset( fmt, 0, sizeof(fmt) );
+
+	snprintf( fmt, sizeof(fmt), inp );
+ 
+	if ((check != 0x04030201) && (check != 0xcafebabe))
+		printf ("\nYou are on the right way !\n");
+	printf( "Check=0x%x\n", check );
+ 
+	if (check==0xcafebabe)
+   	{
+		printf("Yeah dude! You win!\nHere is your girl...\n");
+     		system("/bin/cat flag.txt");
+   	}
+}
+int main(int argc, char *argv[]){
+	char name[1024];
+	char pass[1024];
+	setvbuf(stdout, NULL, _IONBF, 0);
+	printf("Please provide your name: ");
+	scanf("%s",name);
+	phase1(name);
+	printf("\nAnd password: ");
+	scanf("%s", pass);
+	phase2(pass);
+	return 0;
+}
